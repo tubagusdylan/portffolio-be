897ce57a8a56c601e181dde8d879f37e64fa5fa0
tubagusdylan <tubagus.dylanr@gmail.com>
2024-11-16 12:03:32 +0700

    fix: relation table

--- migration/PORTFOLIO-ERD.sql
+++ migration/PORTFOLIO-ERD.sql
@@ -10,48 +10,53 @@ CREATE TABLE `users` (
 
 CREATE TABLE `blogs` (
   `id` varchar(36) PRIMARY KEY,
-  `writer_id` varchar(36) REFERENCES users(id) ON DELETE CASCADE,
+  `writer_id` varchar(36),
   `title` varchar(64),
   `body` text,
   `category` varchar(64),
   `tags` varchar(64),
   `created_at` timestamp DEFAULT NOW(),
-  `updated_at` timestamp DEFAULT NOW()
+  `updated_at` timestamp DEFAULT NOW(),
+  FOREIGN KEY (writer_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE `blog_images` (
   `id` varchar(36) PRIMARY KEY,
-  `blog_id` varchar(36) REFERENCES blogs(id) ON DELETE CASCADE,
+  `blog_id` varchar(36),
   `name` varchar(64),
   `alt` varchar(64),
-  `created_at` timestamp DEFAULT NOW()
+  `created_at` timestamp DEFAULT NOW(),
+  FOREIGN KEY (blog_id) REFERENCES blogs(id) ON DELETE CASCADE
 );
 
 CREATE TABLE `my_projects` (
   `id` varchar(36) PRIMARY KEY,
-  `user_id` varchar(36) REFERENCES users(id) ON DELETE CASCADE,
+  `user_id` varchar(36),
   `title` varchar(64),
   `tech_stack` varchar(64),
   'github_url' varchar(512),
   'web_url' varchar(512),
-  `created_at` timestamp DEFAULT NOW()
+  `created_at` timestamp DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE `project_images` (
   `id` varchar(36) PRIMARY KEY,
-  `project_id` varchar(36) REFERENCES my_projects(id) ON DELETE CASCADE,
+  `project_id` varchar(36),
   `name` varchar(64),
   `alt` varchar(64),
-  `created_at` timestamp DEFAULT NOW()
+  `created_at` timestamp DEFAULT NOW(),
+  FOREIGN KEY (project_id) REFERENCES my_projects(id) ON DELETE CASCADE
 );
 
 CREATE TABLE `testimonies` (
   `id` varchar(36) PRIMARY KEY,
-  `user_id` varchar(36) REFERENCES users(id) ON DELETE CASCADE,
+  `user_id` varchar(36),
   `client_name` varchar(64),
   `description` text,
   `rating` int,
-  `created_at` timestamp DEFAULT NOW()
+  `created_at` timestamp DEFAULT NOW(),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 -- ALTER TABLE `blogs` ADD FOREIGN KEY (`writer_id`) REFERENCES `users` (`id`);
